860c10cec264bb5b5648fbf43c30ba58e8b28610
Lyon <lyon.b.lee@gmail.com>
2023-07-06 22:31:08 -0400

    Lesson 2

--- lesson2.py
+++ lesson2.py
@@ -56,16 +56,72 @@
 # Problem 1:
 # Write a Python function that takes a string as input and returns the number of vowels in the string. Test the function by asking the user for a string and displaying the number of vowels in it.
 
-def num_vowels(word):
-  vowels = "aeiou"
-  num_vowel = 0
+# def num_vowels(word):
+#   vowels = "aeiou"
+#   num_vowel = 0
 
-  for letter in word.lower():
-    for vowel in vowels:
-      if letter == vowel:
-        num_vowel += 1
+#   for letter in word.lower():
+#     for vowel in vowels:
+#       if letter == vowel:
+#         num_vowel += 1
   
-  return f"The number of vowels in the string is: {num_vowel}"
+#   return f"The number of vowels in the string is: {num_vowel}"
 
-string = input("Please enter a string: ")
-print(num_vowels(string))
+# string = input("Please enter a string: ")
+# print(num_vowels(string))
+
+
+
+### OPTIMIZED SOLUTION### 
+# def num_vowels(word):
+#     vowels = "aeiou"
+#     num_vowel = sum(word.lower().count(vowel) for vowel in vowels)
+#     return f"The number of vowels in the string is: {num_vowel}"
+
+
+
+
+# Problem 2:
+# Write a Python function that calculates the area of a circle. The function should take the radius of the circle as input and return the area. Test the function by asking the user for the radius of a circle and displaying the calculated area.
+
+# import math
+
+# def circle_area(radius): 
+#   return radius * math.pi ** 2
+
+# radius = int(input("Please enter a radius of a cirle: "))
+# print(circle_area(radius))
+
+# Problem 3:
+# Write a Python function that checks whether a number is prime. The function should take an integer as input and return True if the number is prime and False otherwise. Test the function by asking the user for a number and displaying whether it is prime or not.
+
+# import math
+
+# def isPrime(num):
+#   if num < 2:
+#     return False
+
+#   # Check for divisibility up to the square root of num
+#   for i in range(2, int(math.sqrt(num)) + 1):
+#     if num % i == 0:
+#       return False
+  
+#   return True
+
+# number = int(input("Please enter a number to check whether it's a prime: "))
+# print(isPrime(number))
+
+# Problem 5:
+# Write a Python function that calculates the factorial of a given number. The function should take an integer as input and return the factorial. Test the function by asking the user for a number and displaying its factorial.
+
+# def factorial(num):
+
+#   fact = 1
+
+#   for i in range(1, num + 1):
+#     fact *= i
+  
+#   return fact
+
+# number = int(input("Please enter a number: "))
+# print(factorial(number))
